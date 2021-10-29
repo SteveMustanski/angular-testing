292b19d933bf3bf35ec4724a0224047c7ac284fe
SteveMustanski <steve.mustanski@gmail.com>
2021-10-29 10:52:22 -0400

    moved service set up to before each

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,21 +3,28 @@ import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
 
-  it('should add two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
+  let calculator: CalculatorService;
+  let loggerSpy: any;
 
+  beforeEach(() => {
+
+    // creates a fake logger service with a fake log method
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(loggerSpy);
+  })
+
+  it('should add two numbers', () => {
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
-
   it('should subract two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
-
     const result = calculator.subtract(2, 2);
 
     expect(result).toBe(0);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
 })
